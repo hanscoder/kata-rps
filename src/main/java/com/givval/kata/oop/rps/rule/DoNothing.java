e333faa1c0b70e22e64e1b8edd2ad5e8baf1cae2
hanscoder <hanssowa@gmail.com>
2015-03-19 21:41:26 +0100

    [Refactoring] Extract doNothing Rule to an own class.

---
+++ src/main/java/com/givval/kata/oop/rps/rule/DoNothing.java
@@ -0,0 +1,19 @@
+package com.givval.kata.oop.rps.rule;
+
+import com.givval.kata.oop.rps.Result;
+import com.givval.kata.oop.rps.Shape;
+import com.givval.kata.oop.rps.rule.Rule;
+
+public class DoNothing implements Rule {
+
+    private static DoNothing doNothing = new DoNothing();
+
+    public static DoNothing doNothing() {
+        return doNothing;
+    }
+
+    @Override
+    public void evaluate(Shape oponent, Result result) {
+        // do nothing
+    }
+}
